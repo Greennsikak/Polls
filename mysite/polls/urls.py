4e46fdc24df079e0b21b18dd1b7e56bb71f12bb7
Nsikak Etim <nsikaketim744@gmail.com>
2020-06-28 14:28:50 +0100

    views.py and urls.py updated

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail')
+    
 ]
 
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -7,6 +7,10 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
