eba672131c7a4c566786d617a3692cd8afb2cef9
Nsikak Etim <nsikaketim744@gmail.com>
2020-07-05 13:59:20 +0100

    Results and details page added

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,8 +16,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}"
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -28,6 +28,6 @@ def vote(request, question_id):
 
     selected_choice.votes += 1
     selected_choice.save()
-    return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
 
 
