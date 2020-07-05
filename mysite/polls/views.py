97162fbcd7311d527aa7c97c768bc564a9df6035
Nsikak Etim <nsikaketim744@gmail.com>
2020-07-05 13:13:05 +0100

    added voting

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -13,4 +14,20 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question})
+
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+
 
